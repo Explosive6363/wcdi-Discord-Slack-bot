a03ed6f62997d41e59386e9099adb850752f219f
hashy0917 <oohasi3939@gmail.com>
2020-09-19 15:43:30 +0000

    add Entry/Exit monitoring

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ from datetime import datetime, timedelta
 #bot = discord.Bot(command_prefix='/')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
+server = os.environ['SERVER_ID']
+channel = os.environ['CHANNEL_ID']
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if member.guild.id == 'SERVER_ID' and (before.channel != after.channel):
+    if member.guild.id == server and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel('CHANNEL_ID')
+        alert_channel = client.get_channel(channel)
         if before.channel is None:
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
