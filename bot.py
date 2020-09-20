a326cf1732bd9b96d31044b406c172100a1f8af8
Taisuke1221 <n4n0k4f0rit@gmail.com>
2020-09-20 14:26:15 +0900

    Fix room access message

--- bot.py
+++ bot.py
@@ -14,12 +14,16 @@ slack = slackweb.Slack(url=webhook)
 @client.event
 async def on_voice_state_update(member, before, after):
     now = datetime.utcnow() + timedelta(hours=9)
+    if before.channel == after.channel:
+        pass
     if before.channel is None:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+        slack.notify(text=msg)
     elif after.channel is None:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+        slack.notify(text=msg)
     else :
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から {after.channel.name} に移動しました。'
-    slack.notify(text=msg)
+        slack.notify(text=msg)
 
 client.run(token)
