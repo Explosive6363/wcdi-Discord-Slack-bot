add261a1e38eb2e8819abb04c79bcbbdc691dce5
Gamou-T <47162587+Gamou-T@users.noreply.github.com>
2020-09-20 18:08:50 +0900

    fix early return method

--- bot.py
+++ bot.py
@@ -15,15 +15,13 @@ slack = slackweb.Slack(url=webhook)
 async def on_voice_state_update(member, before, after):
     now = datetime.utcnow() + timedelta(hours=9)
     if before.channel == after.channel:
-        pass
+        return
     elif before.channel is None:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-        slack.notify(text=msg)
     elif after.channel is None:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
-        slack.notify(text=msg)
-    else :
+    else:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から {after.channel.name} に移動しました。'
-        slack.notify(text=msg)
+    slack.notify(text=msg)
 
 client.run(token)
